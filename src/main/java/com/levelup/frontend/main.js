94aca51223545646f292170c1fb6c3e6a54b0be1
LV52016 <wynkov@gmail.com>
2023-09-29 18:38:33 +0200

    Organized files

--- src/main/java/com/levelup/frontend/main.js
+++ src/main/java/com/levelup/frontend/main.js
@@ -1,1 +1,20 @@
-document.body.innerHTML = "inner";
+function checkInformation() {
+	var api = Java.type("backend.BackendAPI()");
+	alert("API loaded");
+
+	var type = document.getElementById("card-type").value;
+	var num = document.getElementById("card-number").value;
+	var date = document.getElementById("card-date").value;
+	var cvv = document.getElementById("card-cvv").value;
+
+	// Change status text and color according to API return value
+	if (api.checkInformation(type, num, date, cvv) == true) {
+		document.getElementById("status").style.color = "green";
+		document.getElementById("status").innerHTML = "Status: Success!";
+	} else {
+		document.getElementById("status").style.color = "red";
+		document.getElementById("status").innerHTML = "Status: Failed!";
+	}
+
+	alert("API function called");
+}
